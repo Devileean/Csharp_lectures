3824d9c8a94b26ce4d838a748590788c750e0801
AlexseyLoginov <devileean@gmail.com>
2022-11-11 01:35:14 +0300

    example 2 complete

---
+++ Lecture_01/Example02_HelloUser/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
+//можно всё в одну строку
+
+// Console.Write("Введите ваше имя ");
+// string username1 = Console.ReadLine();
+// Console.Write("Привет, ");
+// Console.Write(username);
