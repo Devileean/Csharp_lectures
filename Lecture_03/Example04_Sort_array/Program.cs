f1d2c2249da7df4a9e0361209e60ba621f7eb905
AlexseyLoginov <devileean@gmail.com>
2022-11-24 01:52:32 +0300

    added Example 04 lecture 3 sorts_Array

---
+++ Lecture_03/Example04_Sort_array/Program.cs
@@ -0,0 +1,37 @@
+﻿// функция с циклом сортировки чисел
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 5, 9}; //некий массив чисел, могут повторятся
+
+void PrintArray(int[] array) // метод вывовда массива
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) // метод который будет упорядочивать наш массив
+{
+    for (int i = 0; i < array.Length ; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length - 1; j++) //ищет максимальную цифру
+        {
+            if (array[j] < array[minPosition]) 
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
