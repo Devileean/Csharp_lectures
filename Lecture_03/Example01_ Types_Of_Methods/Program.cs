44e48aa517e550ff48b2f983d97644d0da0bd419
AlexseyLoginov <devileean@gmail.com>
2022-11-24 00:21:42 +0300

    added Example 01 lectures 3 cycles while and for

--- Lecture_03/Example01_ Types_Of_Methods/Program.cs
+++ Lecture_03/Example01_ Types_Of_Methods/Program.cs
@@ -46,7 +46,23 @@ Console.WriteLine(year);
 
 // Вид 4 -> что-то принимают, что-то возвращают. (Самая важная группа методов)
 
+//пример 1 (цикл while)
 string Method4(int count, string text)
+{
+    string result = String.Empty; //класть сюда конечный результат,
+                                  // тоже самое что и string result = "";
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(7, "много текста, ");
+Console.WriteLine(res);
+
+//пример 2 (цикл for)
+string Method41(int count, string text)
 {
     int i = 0;
     string result = String.Empty; //класть сюда конечный результат,
@@ -59,5 +75,5 @@ string Method4(int count, string text)
     return result;
 }
 
-string res = Method4(7, "много текста, ");
-Console.WriteLine(res);
+string res1 = Method41(5, "many text, ");
+Console.WriteLine(res1);
