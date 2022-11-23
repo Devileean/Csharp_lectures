d15d0f4a1d07867108951ab82f2c11002e24cc14
AlexseyLoginov <devileean@gmail.com>
2022-11-23 22:38:30 +0300

    added Example 03 lectures 2

---
+++ Lecture_02/Example03_ArrayCycle/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // если добавляем эту запись, то программа останавливается на первой 18
+    }
+    index++;
+}
